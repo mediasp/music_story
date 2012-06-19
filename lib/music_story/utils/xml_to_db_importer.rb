1e6ac5de5d102bdac0eb40d76e6664a7f8aa0dd1
Nick Griffiths <nick@mediasp.com>
2012-06-19 13:07:21 +0100

    changes to import script:
     - wrap each artist operation in a transaction
     - recover from errors when updating individual artists
     - log successes and failures, print these out after import

--- lib/music_story/utils/xml_to_db_importer.rb
+++ lib/music_story/utils/xml_to_db_importer.rb
@@ -3,27 +3,41 @@ module MusicStory
     def self.import_file_into_db(filename, db, create_tables=false)
       sequel_repos = Repository::Sequel.new(db)
       sequel_repos.create_tables! if create_tables
-      Repository::ArtistXMLFile.new_with_open_file(filename) do |xml_repo|
+      results = Repository::ArtistXMLFile.new_with_open_file(filename) do |xml_repo|
         new(xml_repo, sequel_repos).import
       end
-      return sequel_repos
+
+      return results.merge(:sequel_repos => sequel_repos)
     end
 
     def initialize(xml_repo, sequel_repos)
       @xml_repo = xml_repo
       @sequel_repos = sequel_repos
     end
-    
+
     def import
+      failures = []
+      successes = []
+
       @xml_repo.each do |artist|
-        artist.all_associated_artists.each do |a|
-          @sequel_repos.artist_repo.store(a)
+        begin
+          @sequel_repos.artist_repo.transaction do
+            artist.all_associated_artists.each do |a|
+              @sequel_repos.artist_repo.store(a)
+            end
+            artist.all_genres.each do |g|
+              @sequel_repos.genre_repo.store(g)
+            end
+            @sequel_repos.artist_repo.store(artist)
+          end
+        rescue => e
+          failures << [artist, e]
         end
-        artist.all_genres.each do |g|
-          @sequel_repos.genre_repo.store(g)
-        end
-        @sequel_repos.artist_repo.store(artist)
+
+        successes << artist
       end
+
+      {:successes => successes, :failures => failures}
     end
   end
 end
--- test/test_import.rb
+++ test/test_import.rb
@@ -3,14 +3,15 @@ require 'logger'
 
 describe "MusicStory::Utils::XMLToDBImporter" do
   include MusicStory::TestHelpers
-  
+
   it "should import artist xml file into a new database" do
     logger = ENV['DEBUG'] && Logger.new(STDOUT)
     @db = Sequel.sqlite(:logger => logger)
 
-    sequel_repos = MusicStory::Utils::XMLToDBImporter.import_file_into_db(test_xml_filename, @db, true)
+    result = MusicStory::Utils::XMLToDBImporter.import_file_into_db(test_xml_filename, @db, true)
+    sequel_repos = result[:sequel_repos]
     artist = sequel_repos.artist_repo.get_by_id(42)
-    
+
     assert_artist_correct_from_test_xml_file(artist)
   end
 end
